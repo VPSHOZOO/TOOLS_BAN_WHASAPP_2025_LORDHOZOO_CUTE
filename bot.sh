b566d26d71db52edc348596fa863f2e802b23650
VPSHOZOO <lordhozoo891@gmail.com>
2025-05-04 14:32:50 +0700

    Create bot.sh

---
+++ bot.sh
@@ -0,0 +1,32 @@
+#TEAM SYSTEM UMBRELLA DRAK 999 
+#AUTHOR : LORDHOZOO
+#DILIRIS : 25-05-4 
+#YOUTUBE :LORDHOZOO
+#TIKTOK : LORDHOZOO
+#PESAN : JANGAN DI EDIT HAK SAYA SAYA PANTAU ANDA 
+e="echo -e"
+clear
+$e "=========================================="
+$e "=   INSTALL MODULE JS KAK LORDHOZOO          ="
+$e "=========================================="
+sleep 0.25
+clear
+npm install node-telegram-bot-api
+clear
+npm install nodemailer
+clear
+npm install fs
+clear
+npm install node-telegram-bot-api nodemailer
+clear
+$e "=========================================="
+$e "=   PENGINSTALAN SELESAI KAK LORDHOZOO         ="
+$e "=========================================="
+sleep 0.25
+clear
+$e "=========================================="
+$e "=   MEMULAI BOT KAK LORDHOZOO CUTE        ="
+$e "=========================================="
+sleep 0.25
+clear
+node bot.js
